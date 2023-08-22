a62f7b0ae5489777baef7bc087d409238306fe1a
Rebelzob <karimdavid07@gmail.com>
2023-08-22 21:06:13 +0100

    created bookSlice with addbook and remove reducers

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: {
+    books: [],
+  },
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      const itemId = action.payload;
+      state.books = state.books.filter((book) => book.id !== itemId);
+    },
+  },
+});
+
+export const {
+  addBook,
+  removeBook,
+} = booksSlice.actions;
+
+export default booksSlice.reducer;
