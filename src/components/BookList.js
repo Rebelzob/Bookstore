3a7aedcec1038dc79ec453117119795ebc386192
Rebelzob <karimdavid07@gmail.com>
2023-08-24 14:47:56 +0100

    removed item object and added Redux logic

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,37 +1,21 @@
+import { useSelector } from 'react-redux';
 import BookDetails from './BookDetails';
 import '../assets/styles/bookList.css';
 
 function BookList() {
-  const books = [
-    {
-      id: 1,
-      title: 'The Hobbit',
-      author: 'J.R.R. Tolkien',
-      category: 'Fantasy',
-      chapter: 5,
-      percentage: 50,
-    },
-    {
-      id: 2,
-      title: 'Star Wars',
-      author: 'George Lucas',
-      category: 'Sci-Fi',
-      chapter: 7,
-      percentage: 55,
-    },
-    {
-      id: 3,
-      title: 'The Chronicles of Narnia',
-      author: 'C.S. Lewis',
-      category: 'Fantasy',
-      chapter: 3,
-      percentage: 25,
-    },
-  ];
+  const { books } = useSelector((state) => state.books);
   return (
     <div className="book-container">
       {books.map((book) => (
-        <BookDetails key={book.id} book={book} />
+        <BookDetails
+          id={book.id}
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          category={book.category}
+          chapter={book.chapter}
+          percentage={book.percentage}
+        />
       ))}
     </div>
   );
